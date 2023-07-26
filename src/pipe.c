4557f42832bf50cc93eea2ebcb43edb8bdae2514
Thong Zi qi <tzi-qi@u90z03s03.42kl.edu.my>
2023-07-26 19:53:40 +0800

    [Update] Simpliefied Children.c

--- src/pipe.c
+++ src/pipe.c
@@ -6,7 +6,7 @@
 /*   By: tzi-qi <tzi-qi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 17:25:06 by lchew             #+#    #+#             */
-/*   Updated: 2023/07/26 16:44:19 by tzi-qi           ###   ########.fr       */
+/*   Updated: 2023/07/26 19:52:57 by tzi-qi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,44 +54,11 @@ void	right_child(int *pipe, t_tree *node, char **envp, t_root *sh)
 	ft_dup2(sh->stdin_tmp, STDIN_FILENO);
 }
 
-/**
- * children - Handles the child processes in a binary syntax tree (BST)
- *            traversal, creating pipes, forking child processes, and
- *            waiting for them to complete.
- *
- * @param node: A pointer to the current node in the BST.
- * @param envp: The current environment variables.
- *
- * @returns void.
- */
-void	children(t_tree *node, char **envp, t_root *sh)
+void	children_parent(t_tree *node, char **envp, t_root *sh)
 {
 	pid_t	children[2];
 	int		status;
 
-	if (node->left == NULL)
-	{
-		printf("minishell: syntax error near unexpected token `|'\n");
-		return ;
-	}
-	if (node->right == NULL)
-	{
-		printf("minishell: syntax error: unexpected end of file\n");
-		return ;
-	}
-	ft_pipe(sh->pipe);
-	if (node->left->token == HEREDOC)
-	{
-		ft_dup2(sh->pipe[1], STDOUT_FILENO);
-		recurse_bst(node->left, envp, sh);
-		ft_dup2(sh->stdout_tmp, STDOUT_FILENO);
-	}
-	if (node->right->token == HEREDOC)
-	{
-		ft_dup2(sh->pipe[0], STDIN_FILENO);
-		recurse_bst(node->right, envp, sh);
-		ft_dup2(sh->stdin_tmp, STDIN_FILENO);
-	}
 	children[0] = ft_fork();
 	if (children[0] == 0)
 	{
@@ -112,4 +79,47 @@ void	children(t_tree *node, char **envp, t_root *sh)
 	sh->pipe[0] = 0;
 	waitpid(children[0], &status, 0);
 	waitpid(children[1], &status, 0);
+}
+
+void	heredoc_check(t_tree *node, char **envp, t_root *sh)
+{
+	ft_pipe(sh->pipe);
+	if (node->left->token == HEREDOC)
+	{
+		ft_dup2(sh->pipe[1], STDOUT_FILENO);
+		recurse_bst(node->left, envp, sh);
+		ft_dup2(sh->stdout_tmp, STDOUT_FILENO);
+	}
+	if (node->right->token == HEREDOC)
+	{
+		ft_dup2(sh->pipe[0], STDIN_FILENO);
+		recurse_bst(node->right, envp, sh);
+		ft_dup2(sh->stdin_tmp, STDIN_FILENO);
+	}
+}
+
+/**
+ * children - Handles the child processes in a binary syntax tree (BST)
+ *            traversal, creating pipes, forking child processes, and
+ *            waiting for them to complete.
+ *
+ * @param node: A pointer to the current node in the BST.
+ * @param envp: The current environment variables.
+ *
+ * @returns void.
+ */
+void	children(t_tree *node, char **envp, t_root *sh)
+{
+	if (node->left == NULL)
+	{
+		printf("minishell: syntax error near unexpected token `|'\n");
+		return ;
+	}
+	if (node->right == NULL)
+	{
+		printf("minishell: syntax error: unexpected end of file\n");
+		return ;
+	}
+	heredoc_check(node, envp, sh);
+	children_parent(node, envp, sh);
 }
